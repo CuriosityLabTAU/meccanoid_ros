93e6ee1e22f210119ec4e218ba60529266d320f1
gorengordon <goren@gorengordon.com>
2016-02-04 21:29:16 +0200

    deployment

--- scripts/meccanoid.py
+++ scripts/meccanoid.py
@@ -5,60 +5,61 @@ from std_msgs.msg import String
 import serial
 import os
 import time
+import io
 
 
 class meccanoid():
     ser = None
+    port = ""
 
     def start_serial(self):
-        port = '/dev/ttyACM0'
+        self.port = '/dev/ttyACM0'
         print("Waiting for meccanoid to be connected via arduino ...")
-        while not os.path.exists(port):
+        while not os.path.exists(self.port):
             pass
-        os.system("sudo chmod 666 " + port)
+        os.system("sudo chmod 666 " + self.port)
         time.sleep(1)
-        self.ser = serial.Serial(port, 9600)  # open serial port
+
+        self.ser = serial.Serial(self.port, 9600)  # open serial port
         time.sleep(1)
         print("Arduino connected: ", self.ser.name)# check which port was really used
 
-
-    def close_serial(self):
-        self.ser.close()             # close port
-        pass
-
-    def talker(self):
-        #pub = rospy.Publisher('chatter', String, queue_size=10)
-        #rospy.init_node('talker', anonymous=True)
-        pass
-
-    def callback(self, data):
-        print(data.data)
-        self.ser.write(str.encode(data.data + "\n"))     # write a string
-        time.sleep(1)
-        print("--- done ---")
-
-        # LIM, if data.data==300
-
-    def listener(self):
-
-        # In ROS, nodes are uniquely named. If two nodes with the same
-        # node are launched, the previous one is kicked off. The
-        # anonymous=True flag means that rospy will choose a unique
-        # name for our 'listener' node so that multiple listeners can
-        # run simultaneously.
         rospy.init_node('meccanoid_listener')
         rospy.on_shutdown(self.close_serial)
 
+    def close_serial(self):
+        self.ser.close()             # close port
+
+    def talker(self):
+        pub = rospy.Publisher('from_meccanoid', String, queue_size=10)
+        # rospy.spin()
+        while True:
+            data = self.ser.readline()
+            pub.publish(data)
+            #print(data)
+
+    def callback(self, data):
+        self.ser = serial.Serial(self.port, 9600)  # open serial port
+        # num = str.split(data.data, ',')
+        # check_sum = 0
+        # for i in num:
+        #     check_sum += int(i)
+        # msg = '400,' + data.data + ',' + str(check_sum)
+        # print(msg)
+        # # ==============
+        msg = data.data;
+        self.ser.write(str.encode(msg))     # write a string
+        #time.sleep(0.5)
+
+    def listener(self):
         rospy.Subscriber("to_meccanoid", String, self.callback)
 
-        # spin() simply keeps python from exiting until this node is stopped
-        rospy.spin()
 
 if __name__ == '__main__':
     try:
         m = meccanoid()
         m.start_serial()
-        #m.talker()
         m.listener()
+        m.talker()
     except rospy.ROSInterruptException:
         pass
